e4d935e33e0bd0c8eebf2e9f7d4ede68f6c3247b
samuel21695 <samuel21695@gmail.com>
2023-11-07 22:51:08 +0900

    ✨ feat: forEach()

--- app.js
+++ app.js
@@ -15,5 +15,12 @@ export default function(tagName, styleObject) {
   const element = document.createElement(tagName);
   // Javascript에서 지원하는 keys() 메서드를 활용해, 간편하게 배열로 가공한다.
   let styleObjectKeyArray = Object.keys(styleObject);
-
+  // 배열에 있는 값을 '반복문을 통해' 활용한다는 의미에서 forEach()를 사용한다.
+  // 아래의 forEach() 콜백함수 key는 사실상 배열의 원소가 된다.
+  // 이러한 key와 같은 매개변수를 '반복하는 동안' 사용하는 것으로 '반복자(iterator)'라고 한다.
+  // 개발자들은 이러한 것을 '인자를 전달한다'고 표현한다.
+  styleObjectKeyArray.forEach((key) => {
+    element.style[key] = styleObject[key];
+  });
+  return element;
 }
